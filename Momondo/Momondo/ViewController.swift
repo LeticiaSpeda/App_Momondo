b25342e523d3ee3d841a1f4da7dad9624d285470
Leticia Speda <leticiaspeda@gmail.com>
2023-04-11 14:25:21 -0300

    Configurando CI do projeto

---
+++ Momondo/Momondo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Momondo
+//
+//  Created by Leticia Speda on 09/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .red
+    }
+
+
+}
+
