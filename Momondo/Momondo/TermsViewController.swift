2b54882b79cc8011d70b59c69d1bb6c047daab8c
Leticia Speda <leticiaspeda@gmail.com>
2023-04-17 22:14:11 -0300

    feat: criado tela de aceitacao de termos: Description: - apos o usuario clicar em aceitar os termos sera direcionado a tela de login

--- Momondo/Momondo/TermsViewController.swift
+++ Momondo/Momondo/TermsViewController.swift
@@ -7,13 +7,131 @@
 
 import UIKit
 
-class TermsViewController: UIViewController {
+final class TermsViewController: UIViewController {
+    
+    private lazy var mainVStack: UIStackView = {
+        let stack = UIStackView()
+        stack.axis = .vertical
+        stack.spacing = 10
+        stack.translate()
+        return stack
+    }()
+    
+    private lazy var mainHStack: UIStackView = {
+        let stack = UIStackView()
+        stack.axis = .horizontal
+        stack.spacing = 10
+        stack.translate()
+        return stack
+    }()
+    
+    private lazy var logoImage: UIImageView = {
+        let image = UIImage(named: "logoMomondo")
+        let imageView = UIImageView(image: image)
+        imageView.contentMode = .scaleAspectFill
+        imageView.translate()
+        return imageView
+    }()
+    
+    private lazy var termsText: UILabel = {
+        let attributedText = createTermsAttributedText()
+        let label = UILabel()
+        label.attributedText = attributedText
+        label.numberOfLines = 0
+        label.textAlignment = .justified
+        label.translate()
+        return label
+    }()
+    
+    private func createTermsAttributedText() -> NSAttributedString {
+        let normalText = "Ao usar nosso aplicativo, você concorda com nossos Termos e Condições e nossa "
+        let highlightedText = "Politica de Privacidade e Cookies."
+        
+        let attributedText = NSMutableAttributedString()
+        let normalTextAttribute = NSAttributedString(string: normalText, attributes: [
+            .foregroundColor: UIColor.white,
+        ])
+        attributedText.append(normalTextAttribute)
+        
+        let highlightedTextAttribute = NSAttributedString(string: highlightedText, attributes: [
+            .foregroundColor: UIColor.systemBlue
+        ])
+        attributedText.append(highlightedTextAttribute)
+        return attributedText
+    }
+    
+    private lazy var acceptedButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Accepted", for: .normal)
+        button.setTitleColor(.white, for: .normal)
+        button.backgroundColor = #colorLiteral(red: 0.3838245273, green: 0.109461613, blue: 0.7299745083, alpha: 1)
+        button.layer.cornerRadius = 10
+        button.translate()
+        button.addTarget(self, action: #selector(hadleAction), for: .touchUpInside)
+        return button
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        view.backgroundColor = .red
+        commonInit()
     }
-
-
+    
+    @objc func hadleAction() {
+        let controller = LoginViewController()
+        controller.modalPresentationStyle = .overFullScreen
+        present(controller, animated: true)
+    }
+    
+    private func commonInit() {
+        configureHierarchy()
+        configureConstraints()
+        configureStyle()
+    }
+    
+    private func configureHierarchy() {
+        view.addSubview(mainVStack)
+        view.addSubview(mainHStack)
+        
+        mainVStack.addArrangedSubview(logoImage)
+        mainVStack.addArrangedSubview(termsText)
+        
+        mainHStack.addArrangedSubview(acceptedButton)
+    }
+    
+    private func configureConstraints() {
+        NSLayoutConstraint.activate([
+            mainVStack.topAnchor.constraint(
+                equalTo: view.safeAreaLayoutGuide.topAnchor,
+                constant: 10
+            ),
+            mainVStack.leadingAnchor.constraint(
+                equalTo: view.safeAreaLayoutGuide.leadingAnchor,
+                constant: 10
+            ),
+            mainVStack.trailingAnchor.constraint(
+                equalTo: view.safeAreaLayoutGuide.trailingAnchor,
+                constant: -10
+            ),
+            
+            
+            mainHStack.leadingAnchor.constraint(
+                equalTo: view.safeAreaLayoutGuide.leadingAnchor,
+                constant: 10
+            ),
+            mainHStack.trailingAnchor.constraint(
+                equalTo: view.safeAreaLayoutGuide.trailingAnchor,
+                constant: -10
+            ),
+            mainHStack.bottomAnchor.constraint(
+                equalTo: view.safeAreaLayoutGuide.bottomAnchor,
+                constant: -16
+            )
+        ])
+    }
+    
+    private func configureStyle() {
+        view.backgroundColor = #colorLiteral(red: 0.1051880196, green: 0.01547455508, blue: 0.209243536, alpha: 1)
+    }
+    
 }
 
